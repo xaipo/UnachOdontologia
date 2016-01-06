f7e828ca0915c8878e342fd54e3608ad8609d601
cris666panic <cris93_tian@hotmail.com>
2016-01-06 10:12:00 -0500

    estructura del proyecto
    agrego archivos

---
+++ public/javascripts/chirpApp.js
@@ -0,0 +1,99 @@
+var app = angular.module('chirpApp', ['ngRoute', 'ngResource']).run(function($http, $rootScope) {
+  $rootScope.authenticated = false;
+  $rootScope.current_user = 'Guest';
+
+  $rootScope.signout = function(){
+    $http.get('auth/signout');
+    $rootScope.authenticated = false;
+    $rootScope.current_user = 'Guest';
+  };
+});
+
+app.config(function($routeProvider){
+	$routeProvider
+		//the timeline display
+		.when('/', {
+			templateUrl: 'main.html',
+			controller: 'mainController'
+		})
+		//the login display
+		.when('/login', {
+			templateUrl: 'login.html',
+			controller: 'authController'
+		})
+		//the signup display
+		.when('/register', {
+			templateUrl: 'register.html',
+			controller: 'authController'
+		})
+       
+       .when('/estudiante', {
+			templateUrl: 'estudiante.html',
+		})	
+        
+        .when('/historia', {
+			templateUrl: 'historia.html',
+            })	
+            
+       .when('/nuevoPaciente', {
+			templateUrl: 'nuevoPaciente.html',
+			
+		
+		});
+});
+
+app.factory('postService', function($resource){
+  return $resource('/api/posts/:id');
+});
+
+
+app.controller('mainController', function($rootScope,$scope, postService){
+  $scope.posts = postService.query();
+  $scope.newPost = {created_by: '', text: '', created_at: ''};
+
+  
+  $scope.post = function() {
+  $scope.newPost.created_by = $rootScope.current_user;
+  $scope.newPost.created_at = Date.now();
+  postService.save($scope.newPost, function(){
+    $scope.posts = postService.query();
+    $scope.newPost = {created_by: '', text: '', created_at: ''};
+  });
+};
+  
+  
+  
+  
+});
+
+
+app.controller('authController', function($scope, $http, $rootScope, $location){
+	$scope.user = {username: '', password: ''};
+	$scope.error_message = '';
+
+	$scope.login = function(){
+		$http.post('/auth/login', $scope.user).success(function(data){
+			if(data.state == 'success'){
+				$rootScope.authenticated = true;
+				$rootScope.current_user = data.user.username;
+				$location.path('/estudiante');
+			}
+			else{
+				$scope.error_message = data.message;
+			}
+		});
+	};
+
+	$scope.register = function(){
+		$http.post('/auth/signup', $scope.user).success(function(data){
+			if(data.state == 'success'){
+				$rootScope.authenticated = true;
+				$rootScope.current_user = data.user.username;
+				$location.path('/');
+			}
+			else{
+				$scope.error_message = data.message;
+			}
+		});
+	};
+});
