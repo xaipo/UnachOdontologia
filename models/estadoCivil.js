c1254e275fa0077cff8b32e57c29abde53a17c63
cris666panic <cris93_tian@hotmail.com>
2016-09-09 13:49:25 -0500

    registro de antecedentes

--- models/estadoCivil.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');  
-var Schema = mongoose.Schema;
-
-var estado_civilSchema = new Schema({  
-    nombre: String
-});
-
-module.exports = mongoose.model('Estado_Civil', estado_civilSchema); 
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-//get home page 
-router.get('/',function(req,res,next){
-   res.render('index',{title:"chirp"}); 
-});
-
-module.exports=router;
